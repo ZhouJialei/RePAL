bb7df23ceebfc8e2a3bca55c7fec642f14ad5546
dylzjl <dylzjl@gmail.com>
2015-04-07 16:12:29 +0800

    small change

--- PAL/YDChatOverViewController.m
+++ PAL/YDChatOverViewController.m
@@ -23,6 +23,13 @@ static const int ddLogLevel = LOG_LEVEL_INFO;
 @property (nonatomic,strong) YDConversationViewController *conversationVC;
 @property (nonatomic,strong) UITableView *mtableView;
 @property (nonatomic,strong) NSMutableArray* chats;
+
+@property (weak, nonatomic) IBOutlet UIImageView *avatarImage;
+@property (weak, nonatomic) IBOutlet UILabel *lineOne;
+@property (weak, nonatomic) IBOutlet UILabel *lineTwo;
+@property (weak, nonatomic) IBOutlet UIImageView *isNewImage;
+@property (weak, nonatomic) IBOutlet UILabel *numberLabel;
+
 @end
 @implementation YDChatOverViewController
 - (YDAppDelegate *)appDelegate
@@ -33,17 +40,7 @@ static const int ddLogLevel = LOG_LEVEL_INFO;
 {
     [super viewDidLoad];
     [DDLog addLogger:[DDTTYLogger sharedInstance]];
-
-	self.view.backgroundColor=[UIColor whiteColor];
     //Add a UITableView
-    self.mtableView = [[UITableView alloc] initWithFrame:CGRectMake(0,86,ScreenWidth,ScreenHeight - 86-40) style:UITableViewStylePlain];
-    self.mtableView.delegate=self;
-    self.mtableView.dataSource=self;
-    self.mtableView.rowHeight=64;
-    self.mtableView.separatorStyle=UITableViewCellSeparatorStyleSingleLine;
-    self.mtableView.backgroundColor = [UIColor redColor];
-    [self.mtableView setSeparatorColor:[UIColor whiteColor]];
-    [self.view addSubview:self.mtableView];
     [self loadData];
     //Add Observer
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(newMessageReceived:) name:kNewMessage  object:nil];
@@ -107,76 +104,77 @@ static const int ddLogLevel = LOG_LEVEL_INFO;
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-	static NSString *CellIdentifier = @"Cell";
-	
-	UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
-	if (cell == nil)
-        {
-		cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
+    static NSString *CellIdentifier = @"Cell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil)
+    {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
                                       reuseIdentifier:CellIdentifier];
-        }
-	Chat* chat = [self.chats objectAtIndex:indexPath.row];
+    }
+    Chat* chat = [self.chats objectAtIndex:indexPath.row];
     
     XMPPUserCoreDataStorageObject *user = [[self appDelegate ].xmppRosterStorage userForJID:
-                                               [XMPPJID jidWithString:chat.jidString]
-                                                xmppStream:[self appDelegate ].xmppStream
-                                                managedObjectContext:[self appDelegate ]. managedObjectContext_roster];
-
- 	UIView *bgView = [[UIView alloc] initWithFrame:CGRectMake(0,0,320,60)];
+                                           [XMPPJID jidWithString:chat.jidString]
+                                                                                 xmppStream:[self appDelegate ].xmppStream
+                                                                       managedObjectContext:[self appDelegate ]. managedObjectContext_roster];
+    
+    UIView *bgView = [[UIView alloc] initWithFrame:CGRectMake(0,0,320,60)];
     bgView.backgroundColor=[UIColor clearColor];
     if (![[chat isGroupMessage] boolValue])
-          {
-    UIImageView *avatarImage = [[UIImageView alloc] initWithFrame:CGRectMake(6,14,37,37)];
-    avatarImage.backgroundColor=[UIColor clearColor];
-    avatarImage.contentMode=UIViewContentModeScaleAspectFill;
-    UIImage *avImage = [self configurePhotoForCell:cell user:user];
-    avatarImage.image = avImage;
-    avatarImage.layer.cornerRadius = 5.0;
-    avatarImage.layer.masksToBounds = YES;
-    avatarImage.layer.borderColor = [UIColor lightGrayColor].CGColor;
-    avatarImage.layer.borderWidth = 1.0;
-    [bgView addSubview:avatarImage];
-    
-    UIImageView *arrowView = [[UIImageView alloc] initWithFrame:CGRectMake(286,13,27,45)];
-    arrowView.backgroundColor=[UIColor clearColor];
-    arrowView.image=[UIImage imageNamed:@"arrow.png"];
-    [bgView addSubview:arrowView];
-    
-    UILabel *line1 = [[UILabel alloc] initWithFrame:CGRectMake(58,5,220,25)];
-    line1.backgroundColor = [UIColor clearColor];
-    NSString *cleanName = [chat.jidString stringByReplacingOccurrencesOfString:kXMPPServer withString:@""];
-    cleanName=[cleanName stringByReplacingOccurrencesOfString:@"@" withString:@""];
-    line1.text=cleanName;
-    line1.font =   [UIFont systemFontOfSize:18];
-    line1.textColor = [UIColor blackColor] ;
-    [bgView addSubview:line1];
-    if ([chat.isNew  boolValue])
+    {
+        
+        UIImageView *avatarImage = [[UIImageView alloc] initWithFrame:CGRectMake(6,14,37,37)];
+        avatarImage.backgroundColor=[UIColor clearColor];
+        avatarImage.contentMode=UIViewContentModeScaleAspectFill;
+        UIImage *avImage = [self configurePhotoForCell:cell user:user];
+        avatarImage.image = avImage;
+        avatarImage.layer.cornerRadius = 5.0;
+        avatarImage.layer.masksToBounds = YES;
+        avatarImage.layer.borderColor = [UIColor lightGrayColor].CGColor;
+        avatarImage.layer.borderWidth = 1.0;
+        [bgView addSubview:avatarImage];
+        
+        UIImageView *arrowView = [[UIImageView alloc] initWithFrame:CGRectMake(286,13,27,45)];
+        arrowView.backgroundColor=[UIColor clearColor];
+        arrowView.image=[UIImage imageNamed:@"arrow.png"];
+        [bgView addSubview:arrowView];
+        
+        UILabel *line1 = [[UILabel alloc] initWithFrame:CGRectMake(58,5,220,25)];
+        line1.backgroundColor = [UIColor clearColor];
+        NSString *cleanName = [chat.jidString stringByReplacingOccurrencesOfString:kXMPPServer withString:@""];
+        cleanName=[cleanName stringByReplacingOccurrencesOfString:@"@" withString:@""];
+        line1.text=cleanName;
+        line1.font =   [UIFont systemFontOfSize:18];
+        line1.textColor = [UIColor blackColor] ;
+        [bgView addSubview:line1];
+        if ([chat.isNew  boolValue])
         {
-        UIImageView *newImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"new.png"]];
-        newImageView.backgroundColor=[UIColor clearColor];
-        newImageView.frame=CGRectMake(248,16,28,14);
-        [bgView addSubview:newImageView];
-        //int numberOfNewMessages = [self countNewMessagesForJID:currentChatThread.jidString];
-        UILabel *numberLabel = [[UILabel alloc] initWithFrame:CGRectMake(257,13,30,15)];
-        numberLabel.backgroundColor = [UIColor clearColor];
-        numberLabel.textAlignment=NSTextAlignmentRight;
-        numberLabel.font=[UIFont systemFontOfSize:16];
-        numberLabel.textColor=[UIColor blackColor];
-        numberLabel.text=[NSString stringWithFormat:@"%i",[self countNewMessagesForJID:chat.jidString]];
-        [bgView addSubview:numberLabel];
+            UIImageView *newImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"new.png"]];
+            newImageView.backgroundColor=[UIColor clearColor];
+            newImageView.frame=CGRectMake(248,16,28,14);
+            [bgView addSubview:newImageView];
+            //int numberOfNewMessages = [self countNewMessagesForJID:currentChatThread.jidString];
+            UILabel *numberLabel = [[UILabel alloc] initWithFrame:CGRectMake(257,13,30,15)];
+            numberLabel.backgroundColor = [UIColor clearColor];
+            numberLabel.textAlignment=NSTextAlignmentRight;
+            numberLabel.font=[UIFont systemFontOfSize:16];
+            numberLabel.textColor=[UIColor blackColor];
+            numberLabel.text=[NSString stringWithFormat:@"%i",[self countNewMessagesForJID:chat.jidString]];
+            [bgView addSubview:numberLabel];
         }
-    NSString *textForSecondLine = [NSString stringWithFormat:@"%@: %@",[YDHelper dayLabelForMessage:chat.messageDate],chat.messageBody];
-    
-    UILabel *line2 = [[UILabel alloc] initWithFrame:CGRectMake(58,38,220,16)];
-    line2.backgroundColor = [UIColor clearColor];
-    line2.text=textForSecondLine;
-    line2.font =  [UIFont systemFontOfSize:12];
-    line2.textColor = [UIColor blackColor] ;
-    [bgView addSubview:line2];
-          }
+        NSString *textForSecondLine = [NSString stringWithFormat:@"%@: %@",[YDHelper dayLabelForMessage:chat.messageDate],chat.messageBody];
+        
+        UILabel *line2 = [[UILabel alloc] initWithFrame:CGRectMake(58,38,220,16)];
+        line2.backgroundColor = [UIColor clearColor];
+        line2.text=textForSecondLine;
+        line2.font =  [UIFont systemFontOfSize:12];
+        line2.textColor = [UIColor blackColor] ;
+        [bgView addSubview:line2];
+    }
     cell.backgroundView = bgView;
-	
-	return cell;
+    
+    return cell;
 }
  
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
